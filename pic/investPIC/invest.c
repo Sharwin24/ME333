5ebb4b0615e956d9255d8c02d3388d8886dcd0b3
Sharwin24 <sharwin24@gmail.com>
2025-02-03 14:45:21 -0600

    Invest.c runs on PIC

--- pic/investPIC/invest.c
+++ pic/investPIC/invest.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "nu32dip.h"
 #define MAX_YEARS 100
 #define MSG_LEN 25
 
@@ -43,20 +44,25 @@ int getUserInput(Investment* invp) {
 
   // printf("Enter investment, growth rate, number of yrs (up to %d): ", MAX_YEARS);
   NU32DIP_WriteUART1(inputString);
-  sscanf(inputString, "%lf %lf %d", &(invp->inv0), &(invp->growth), &(invp->years));
   // scanf("%lf %lf %d", &(invp->inv0), &(invp->growth), &(invp->years));
   NU32DIP_ReadUART1(inputString, MSG_LEN);
+  sscanf(inputString, "%lf %lf %d", &(invp->inv0), &(invp->growth), &(invp->years));
 
   valid = (invp->inv0 > 0) && (invp->growth > 0) &&
     (invp->years > 0) && (invp->years <= MAX_YEARS);
-  // printf("Valid input?  %d\n", valid);
-  char validInput[MSG_LEN];
-  sprintf(validInput, "Valid input?  %d\r\n", valid);
-  NU32DIP_WriteUART1(validInput);
+
+  // printf("Inv: %10.2f GR: %10.2f Yrs: %3d\n", invp->inv0, invp->growth, invp->years);
+  char invGRYrs[MSG_LEN];
+  sprintf(invGRYrs, "Inv: %10.2f GR: %10.2f Yrs: %3d\r\n", invp->inv0, invp->growth, invp->years);
+  NU32DIP_WriteUART1(invGRYrs);
 
   if (!valid) {
     // printf("Invalid input; exiting.\n");
-    NU32DIP_WriteUART1("Invalid input; exiting.\r\n");
+    NU32DIP_WriteUART1("Invalid input, exiting.\r\n");
+  }
+  else {
+    // printf("Valid input, calculating growth.\n");
+    NU32DIP_WriteUART1("Valid input, calculating growth.\r\n");
   }
   return(valid);
 }
