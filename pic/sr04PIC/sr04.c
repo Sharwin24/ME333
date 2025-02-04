8741d09127f473c2a96e4494aa976bde5f761518
Sharwin24 <sharwin24@gmail.com>
2025-02-03 20:34:53 -0600

    Using difference

--- pic/sr04PIC/sr04.c
+++ pic/sr04PIC/sr04.c
@@ -29,25 +29,24 @@ unsigned int SR04_read_raw(unsigned int timeout) {
   // turn off TRIG
   TRIG = 0;
   // wait until ECHO is on
-  while (!ECHO) { ; }
+  while (!ECHO) {}
   // note the value of the core timer
-  // unsigned int start = _CP0_GET_COUNT();
-  _CP0_SET_COUNT(0);
+  unsigned int start = _CP0_GET_COUNT();
+  // _CP0_SET_COUNT(0);
   // wait until ECHO is off or timeout core ticks has passed
-  while (ECHO && _CP0_GET_COUNT() < timeout) {}
-  // while (ECHO) { ; }
+  while (ECHO && _CP0_GET_COUNT() - start < timeout) {}
   // note the core timer
   unsigned int end = _CP0_GET_COUNT();
   // return the difference in core times
-  return end;
+  return end - start;
 }
 
 float SR04_read_meters() {
   // read the raw rs04 value [core timer ticks]
-  unsigned int raw = SR04_read_raw(CORE_TICKS_PER_SECOND);
+  unsigned int raw = SR04_read_raw(CORE_TICKS_PER_SECOND / 4);
   // convert the time to meters, the velocity of sound in air is 346 m/s
-  float sec_per_tick = 1.0f / ((float)CORE_TICKS_PER_SECOND);
-  float meters = ((float)raw) * (sec_per_tick / 2.0f) * ((float)SOUND_SPEED);
+  float seconds = (float)raw / CORE_TICKS_PER_SECOND;
+  float meters = (seconds * 346.0f) / 2.0f;
   // return the distance in meters
   return meters;
 }
