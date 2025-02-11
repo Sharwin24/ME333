3b4d7ce50df9cdb296fd349c797af49b4b23a0cd
Sharwin24 <sharwin24@gmail.com>
2025-02-10 23:16:01 -0600

    stopwatch

--- pic/stopwatch/main.c
+++ pic/stopwatch/main.c
@@ -6,24 +6,41 @@
 
 #define STOPWATCH_BTN PORTBbits.RB7 // TDI/RPB7/CTED3/PMD5/INT0/RB7
 
+volatile int stopwatch_state = 0; // 0 = stopped, 1 = running
+volatile unsigned int stopwatch_time = 0; // time in seconds
+
 void __ISR(_EXTERNAL_0_VECTOR, IPL2SOFT) Ext0ISR(void) { // step 1: the ISR
   // Debounce by delaying and then checking the button again
   _CP0_SET_COUNT(0);
   while (_CP0_GET_COUNT() < DEBOUNCE_DELAY) { ; } // delay for 10ms
+
   if (!STOPWATCH_BTN) { // if the button is still pressed, it's a real press
-    TRISBCLR = 0x30; // Make TRIS for B4 and B5 0, 
-    NU32DIP_GREEN = 1;    // Turn GREEN and YELLOW off.  These pins sink current
-    NU32DIP_YELLOW = 1;    // on the NU32DIP, so "high" (1) = "off" and "low" (0) = "on"
-    _CP0_SET_COUNT(0);
-    while (_CP0_GET_COUNT() < DELAY_250_MS) { ; } // delay for 250ms
-    NU32DIP_GREEN = 0;    // Turn GREEN and YELLOW on.  These pins sink current
-    NU32DIP_YELLOW = 0;    // on the NU32DIP, so "high" (1) = "off" and "low" (0) = "on"ds
+    if (stopwatch_state == 0) {
+      NU32DIP_WriteUART1("Starting the Stopwatch\r\n");
+      stopwatch_state = 1; // Start the watch
+
+      // Start the timer
+      _CP0_SET_COUNT(0);
+    }
+    else {
+      NU32DIP_WriteUART1("Stopping the Stopwatch\r\n");
+      stopwatch_state = 0; // Stop the watch
+
+      // Stop the timer and record the time
+      stopwatch_time = _CP0_GET_COUNT() / CORE_TICKS; // [sec]
+      char msg[50];
+      sprintf(msg, "Time: %d [s]\r\n", stopwatch_time);
+      NU32DIP_WriteUART1(msg);
+    }
   }
   IFS0bits.INT0IF = 0; // clear interrupt flag IFS0<3>
 }
 
 int main(void) {
   NU32_Startup(); // cache on, min flash wait, interrupts on, LED/button init, UART init
+  NU32DIP_WriteUART1("Press the Stopwatch button\r\n");
+  stopwatch_state = 0; // Stop the watch
+
   __builtin_disable_interrupts(); // step 2: disable interrupts
   INTCONbits.INT0EP = 0; // step 3: INT0 triggers on falling edge
   IPC0bits.INT0IP = 2; // step 4: interrupt priority 2
